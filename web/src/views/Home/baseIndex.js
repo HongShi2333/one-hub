d11d2f3c8772d0e4e171aab55b1c0982fc688539
HongShi2333 <142460819+HongShi2333@users.noreply.github.com>
2024-08-04 09:24:26 +0800

    Update baseIndex.js

--- web/src/views/Home/baseIndex.js
+++ web/src/views/Home/baseIndex.js
@@ -22,16 +22,18 @@ const BaseIndex = () => {
                 One Hub
               </Typography>
               <Typography variant="h4" sx={{ fontSize: '1.5rem', color: '#fff', lineHeight: 1.5 }}>
-                {t('description')}
+                由 不会玩电路的红石 使用one-api搭建的API网站 <br />
+                支持OpenAI以及Google Gemini的部分模型 <br />
+                支持的模型列表请查看 关于 页面
               </Typography>
               <Button
                 variant="contained"
-                startIcon={<GitHub />}
-                href="https://github.com/MartialBE/one-hub"
+                startIcon={<Language />}
+                href="https://home.hongshi.us.kg"
                 target="_blank"
                 sx={{ backgroundColor: '#24292e', color: '#fff', width: 'fit-content', boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)' }}
               >
-                GitHub
+                我的主页
               </Button>
             </Stack>
           </Grid>
